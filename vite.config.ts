3d6cb8f3f54b84fabddc3537d75ce30f29a8a45c
Vita Midori <hi@vitamidori.com>
2026-01-26 12:00:10 +0100

    Automatically deploy to GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/structedit/',
     server: {
       port: 3000,
       host: '0.0.0.0',
